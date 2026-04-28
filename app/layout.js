5ea1989892d0af469adedf20bfc8754457319291
metehan05-eng <metehanhaydarerbas@gmail.com>
2026-04-28 22:12:53 +0300

    Vercel Speed Insights Installation Complete

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,22 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "LangChain UI",
-  description: "The opensource Chat AI toolkit",
+  title: "LifeCoach AI",
+  description: "Yapay Zeka Destekli Kişisel Yaşam Koçunuz",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="tr">
       <body className={inter.className}>
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
