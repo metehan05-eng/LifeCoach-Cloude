29da19478ecd1a1205e2beedc3d6d0c184810fa0
metehan05-eng <metehanhaydarerbas@gmail.com>
2026-05-11 18:56:22 +0300

    Deleted large redundant scripts directory to reduce project size

--- scripts/test-youtube.js
+++
@@ -1,4 +0,0 @@
-const YOUTUBE_API_KEY = "AIzaSyCPBvIRhLXGByRcLpu_lnne7ZCbCYsKkzk";
-fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=test&relevanceLanguage=tr&key=${YOUTUBE_API_KEY}`)
-.then(res => res.json())
-.then(console.log);
